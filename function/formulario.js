cb45ac8bc40302abfed02f0cbbef992984b01ef2
OpenShift guest <davidvelasquezm.6@gmail.com>
2012-11-27 00:16:32 -0500

    0:16 11/27/2012

--- function/formulario.js
+++ function/formulario.js
@@ -6,24 +6,49 @@ $(document).ready(function() {
     
     
     //Ejemplo para validar los campos
-        var input01_val = new LiveValidation( "input01", { validMessage: "Correct!", wait: 1000} );
-        input01_val.add( Validate.Presence,{ failureMessage: "Ingresa tu nombre" } );
+        var input01_val = new LiveValidation( "input01", { validMessage: "Correct!", wait: 1000 } );
+        input01_val.add( Validate.Presence,{ failureMessage: "Insert Your Name" } );
         
         var input02_val = new LiveValidation( "input02", { validMessage: "Correct!", wait: 1000} );
-        input02_val.add( Validate.Presence,{ failureMessage: "Ingresa tu apellido" } );
+        input02_val.add( Validate.Presence,{ failureMessage: "Insert Your LastName" } );
     
         var input03_val = new LiveValidation( "input03", { validMessage: "Correct!", wait: 1000 } );
-        input03_val.add( Validate.Presence,{ failureMessage: "Ingresa un email" } );
-        input03_val.add( Validate.Email,{ failureMessage: "Debe ser un email válido" } );
+        input03_val.add( Validate.Presence,{ failureMessage: "Insert Your E-mail" } );
+        input03_val.add( Validate.Email,{ failureMessage: "Must Be a Valid E-mail" } );
                 
         var select01_val = new LiveValidation( "select01", { validMessage: "Correct!", wait: 1000} );
-        select01_val.add( Validate.Presence,{ failureMessage: "Escoge el genero" } );
+        select01_val.add( Validate.Presence,{ failureMessage: "Choose A Genre" } );
         
     });
     
     $('#boton-formulario-2').click(function(e) {
         e.preventDefault();
+          //$('.formu').validate();
+          
         var password_val = new LiveValidation('input03');
-        password_val.add( Validate.Confirmation, { match: 'input02' } );        
+        password_val.add( Validate.Confirmation, { match: 'input02' } );   
+        
+        var input01_val = new LiveValidation( "input01", { validMessage: "Correct!", wait: 1000 } );
+        input01_val.add( Validate.Presence,{ failureMessage: "Insert a Username" } );
+        
+        var input01_val = new LiveValidation( "input02", { validMessage: "Correct!", wait: 1000 } );
+        input01_val.add( Validate.Presence,{ failureMessage: "Insert a Password" } );
+        
+        var input01_val = new LiveValidation( "input03", { validMessage: "Correct!", wait: 1000 } );
+        input01_val.add( Validate.Presence,{ failureMessage: "Insert a Passwords" } );
+        
+         var input01_val = new LiveValidation( "input04", { validMessage: "Correct!", wait: 1000 } );
+        input01_val.add( Validate.Presence,{ failureMessage: "Please put The Required Information" } );
+        
+        var input01_val = new LiveValidation( "textarea", { validMessage: "Correct!", wait: 1000 } );
+        input01_val.add( Validate.Presence,{ failureMessage: "Insert a valid Username" } );
+        
+        var input01_val = new LiveValidation( "textarea1", { validMessage: "Correct!", wait: 1000 } );
+        input01_val.add( Validate.Presence,{ failureMessage: "Insert a valid U" } );
+        
+        var input01_val = new LiveValidation( "textarea", { validMessage: "Correct!", wait: 1000 } );
+        input01_val.add( Validate.Presence,{ failureMessage: "Insert a valid Username" } );
     });
+    
+    
 });
